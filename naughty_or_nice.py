bd3b205343f8dba7902e83b5755365b5762e0ce4
I-Am-Crumbles <95263402+I-Am-Crumbles@users.noreply.github.com>
2022-03-12 21:44:16 -0600

    Update naughty_or_nice.py

--- naughty_or_nice.py
+++ naughty_or_nice.py
@@ -10,17 +10,25 @@ import sys
 
 ### HELPER FUNCTIONS (IF NECESSARY) ###
 def naughty_or_nice(santa_list):
+  # defines a function called and it's parameters
   goodlist = []
+  # empty list variable for use later
   for x in santa_list:
+    # itterates through the parameter passed to the function
     goodlist.append(x.split()[-1])
+    # appends each name in the file to a new list
     good_count = {}
+    # creates an empty dictionary
     for i in goodlist: good_count[i] = good_count.get(i,0)+1
+      # itterates through goodlist and counts each occourence of good and bad
   return "Naughty list has " + str(good_count["bad"]) + " people!" + "\n" "Nice list has " + str(good_count["good"]) + " people!"
+  # return statement that returns a string of how many naughty and how many nice people were listed in the file
   
     
 
 ### MAIN FUNCTION ###
 def main():
+  
   file_name = sys.argv[1]
   open_file = open(file_name)
   print(naughty_or_nice(open_file))
