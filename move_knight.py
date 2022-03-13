84fb1078983a93abb6baecb491149c777b636156
I-Am-Crumbles <95263402+I-Am-Crumbles@users.noreply.github.com>
2022-03-12 21:39:22 -0600

    Update move_knight.py
    add brief comment description of the function. Will come back later and be more detailed.

--- move_knight.py
+++ move_knight.py
@@ -1,4 +1,6 @@
 # I realize there is a lot going on here but this is one of the more logicaly difficult problems we had to work on.
+# this is not a script, rather a simple function. It is here to demonstrate the ability to follow a complex list of instructions and logically break it down step by step 
+# to be understood by a computer program
 
 '''Write a function move_knight which takes in a list coords_and_dirs, whose first element is a list, indicating the coordinates of the Knight's starting position, and whose second element is a four-character string, indicating where the Knight should move in one turn.
 
@@ -46,14 +48,23 @@ Else, the function should return a string with updated file and rank, formatted 
 '''
 
 def move_knight(coords_and_dirs:list):
+  # defines the function and it's parameter as explained above
   moves = coords_and_dirs[1]
+  # takes the 2nd argument in the parameter and saves it as a variable "moves" 
   coords = coords_and_dirs[0]
+  # takes the first argument in the parameter and saves it as a variable "coords"
   new_coords =[]
+  # creats an empty list called new_coords to be used later
   if int(moves[1]) == 0 or int(moves[3]) == 0:
+    # converts string entered that respresents the knights move to an integer and checks if they are equal to zero
     return("Illegal Move - Horsey Down!")
+  #if the above statement is true this line tell the function to return a string explaining that the knight can not make that move
   if int(moves[1]) + int(moves[3]) != 3: # THis is genious - Ned
+    # checks to make sure that the sum of the moves made is equal to 3 (knights move 3 spaces on a chess board)
     return("Illegal Move - Horsey Down!") 
+    # returns a statement telling you the move was illegal if the total spaces moved does not equal 3
   if moves[0] == "U" and moves[2] == "R":
+    # if statement checking the direction of the moves
     if int(moves[1]) + int(moves[3]) == 3:
       new_coords.append(int(moves[1]) + coords[1])
       new_coords.append(int(moves[3]) + coords[0]) 
@@ -63,7 +74,9 @@ def move_knight(coords_and_dirs:list):
         return "Illegal Move - Horsey Down!"
       else:
         return "The knight has moved to " + str(new_coords) + "."
+      #lines 68 - 76 determine if the move made places the knight in a valid spot on the board, if it doesn't it returns an error statement, if it does it returns the knights new coordinates on the board
   if moves[0] == "U" and moves[2] == "L":
+    # if statement checking the direction of the moves
     if int(moves[1]) + int(moves[3]) == 3:
       new_coords.append(coords[0] - int(moves[3]))
       new_coords.append(int(moves[1]) + coords[1])
@@ -73,7 +86,9 @@ def move_knight(coords_and_dirs:list):
         return "Illegal Move - Horsey Down!"
       else:
         return "The knight has moved to " + str(new_coords) + "." 
+      #lines 80-88 determine if the move made places the knight in a valid spot on the board, if it doesn't it returns an error statement, if it does it returns the knights new coordinates on the board
   if moves[0] == "D" and moves[2] == "R":
+    # if statement checking the direction of the moves
     if int(moves[1]) + int(moves[3]) == 3:
       new_coords.append(int(moves[3]) + coords[0])
       new_coords.append(coords[1] - int(moves[1]))
@@ -82,8 +97,10 @@ def move_knight(coords_and_dirs:list):
       elif new_coords[1] >7 or new_coords[1] <0:
         return "Illegal Move - Horsey Down!"
       else:
-        return "The knight has moved to " + str(new_coords) + "."        
+        return "The knight has moved to " + str(new_coords) + "."   
+      #lines 91-99 determine if the move made places the knight in a valid spot on the board, if it doesn't it returns an error statement, if it does it returns the knights new coordinates on the board
   if moves[0] == "D" and moves[2] == "L":
+    # if statement checking the direction of the moves
     if int(moves[1]) + int(moves[3]) == 3:
       new_coords.append(coords[1] - int(moves[1]))
       new_coords.append(coords[0] - int(moves[3])) 
@@ -93,4 +110,6 @@ def move_knight(coords_and_dirs:list):
         return "Illegal Move - Horsey Down!"
       else:
         return "The knight has moved to " + str(new_coords) + "." 
+      #lines 102-110 determine if the move made places the knight in a valid spot on the board, if it doesn't it returns an error statement, if it does it returns the knights new coordinates on the board
 print(move_knight([[4,5], "D1L2"]))
+#print statement to call and test the function
